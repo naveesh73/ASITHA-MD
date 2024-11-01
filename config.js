484581e437ec7dba6ca211cc5f8972c9d2409573
naveesh73 <naveeshfernando73@gmail.com>
2024-11-01 12:55:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=vzIHDIZb#9UQWN4l5qF68oD-rwCuXDETsi1c8ftZ47rjFDwoQKdk",
+MONGODB: process.env.MONGODB || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=vzIHDIZb#9UQWN4l5qF68oD-rwCuXDETsi1c8ftZ47rjFDwoQKdk",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
 };
